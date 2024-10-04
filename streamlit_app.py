b2bf0f56dd1e6217e212e14a458b0c8bb1bc6375
Pathak2k88 <148472270+Pathak2k88@users.noreply.github.com>
2024-10-04 19:03:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ st.write(
 )
 name_on_order=st.text_input('Name on Smoothie:')
 st.write('Name on your smoothie will be',name_on_order)
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 
 ingredients_list = st.multiselect("Choose upto 5 ingredients",my_dataframe,max_selections=6)
